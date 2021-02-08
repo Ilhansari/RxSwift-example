b3a689489534b7452f0695061529ee7f67260652
Ilhansari <ilhansari94@hotmail.com>
2021-02-08 12:52:31 +0300

    Create persisting objects to disk with RxSwift

--- gitfeed-example/GitFeed/ActivityController.swift
+++ gitfeed-example/GitFeed/ActivityController.swift
@@ -40,11 +40,16 @@ class ActivityController: UITableViewController {
 
   private let events = BehaviorRelay<[Event]>(value: [])
   private let bag = DisposeBag()
+  private let eventsFileURL = cachedFileURL("events.json")
+  private let modifiedFileURL = cachedFileURL("modified.txt")
+  
+  private let lastModified = BehaviorRelay<String?>(value: nil)
 
   override func viewDidLoad() {
     super.viewDidLoad()
     title = repo
 
+    
     self.refreshControl = UIRefreshControl()
     let refreshControl = self.refreshControl!
 
@@ -53,6 +58,14 @@ class ActivityController: UITableViewController {
     refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
     refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
 
+    let decoder = JSONDecoder()
+    if let eventsData = try? Data(contentsOf: eventsFileURL),
+       let persistenceEvents = try? decoder.decode([Event].self, from: eventsData) {
+      events.accept(persistenceEvents)
+    }
+    if let lastModifiedString = try? String(contentsOf: modifiedFileURL, encoding: .utf8) {
+      lastModified.accept(lastModifiedString)
+    }
     refresh()
   }
 
@@ -67,8 +80,12 @@ class ActivityController: UITableViewController {
     let response = Observable.from([repo])
       .map { urlString -> URL in
         return URL(string: "https://api.github.com/repos/\(urlString)/events")!
-      }.map { url -> URLRequest in
-        return URLRequest(url: url)
+      }.map { [weak self] url -> URLRequest in
+        var request = URLRequest(url: url)
+        if let modifiedHeader = self?.lastModified.value {
+          request.addValue(modifiedHeader as String, forHTTPHeaderField: "Last-Modified")
+        }
+        return request
       }.flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)>  in
         return URLSession.shared.rx.response(request: request)
       }
@@ -83,7 +100,25 @@ class ActivityController: UITableViewController {
       }
       .subscribe(onNext: { [weak self] newEvents in
         self?.processEvents(newEvents)
-      }).disposed(by: bag)
+      })
+      .disposed(by: bag)
+    
+    response
+      .filter { response, _ in
+        return 200..<400 ~= response.statusCode
+      }
+      .flatMap { response, _ -> Observable<String> in
+        guard let value = response.allHeaderFields["Last-Modified"] as? String else {
+          return Observable.empty()
+        }
+        return Observable.just(value)
+      }
+      .subscribe(onNext: { [weak self] modifiedHeader in
+        guard let self = self else { return }
+        self.lastModified.accept(modifiedHeader)
+        try? modifiedHeader.write(to: self.modifiedFileURL, atomically: true, encoding: .utf8)
+      })
+      .disposed(by: bag)
   }
   
   func processEvents(_ newEvents: [Event]) {
@@ -96,9 +131,12 @@ class ActivityController: UITableViewController {
       self.tableView.reloadData()
       self.refreshControl?.endRefreshing()
     }
-    
+    let encoder = JSONEncoder()
+    if let eventsData = try? encoder.encode(updatedEvents) {
+      try? eventsData.write(to: eventsFileURL, options: .atomicWrite)
+    }
   }
-
+  
   // MARK: - Table Data Source
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return events.value.count
@@ -114,3 +152,10 @@ class ActivityController: UITableViewController {
     return cell
   }
 }
+
+func cachedFileURL(_ fileName: String) -> URL {
+  return FileManager.default.urls(for: .cachesDirectory, in: .allDomainsMask)
+    .first!
+    .appendingPathComponent(fileName)
+}
+
