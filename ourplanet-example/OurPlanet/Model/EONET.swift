1c590737b48726719735e074ff684e6f0226220a
Ilhansari <ilhansari94@hotmail.com>
2021-02-14 18:34:37 +0300

    Create observable `categories` to map sorted planet name

--- ourplanet-example/OurPlanet/Model/EONET.swift
+++ ourplanet-example/OurPlanet/Model/EONET.swift
@@ -38,47 +38,63 @@ class EONET {
   static let API = "https://eonet.sci.gsfc.nasa.gov/api/v2.1"
   static let categoriesEndpoint = "/categories"
   static let eventsEndpoint = "/events"
-
+  
+  
+  static var categories: Observable<[EOCategory]> = {
+    let request: Observable<[EOCategory]> = EONET.request(endpoint: categoriesEndpoint, contentIdentifier: "categories")
+    return request.map { categories in
+      categories.sorted { $0.name < $1.name }}
+      .catchErrorJustReturn([])
+      .share(replay: 1, scope: .forever)
+  }()
+  
   static func jsonDecoder(contentIdentifier: String) -> JSONDecoder {
     let decoder = JSONDecoder()
     decoder.userInfo[.contentIdentifier] = contentIdentifier
     decoder.dateDecodingStrategy = .iso8601
     return decoder
   }
-
+  
   static func filteredEvents(events: [EOEvent], forCategory category: EOCategory) -> [EOEvent] {
     return events.filter { event in
       return event.categories.contains(where: { $0.id == category.id }) && !category.events.contains {
         $0.id == event.id
       }
-      }
-      .sorted(by: EOEvent.compareDates)
+    }
+    .sorted(by: EOEvent.compareDates)
   }
   
-  static func request(endpoint: String, query: [String: Any] = [:]) -> Observable<[String: Any]> {
+  static func request<T: Decodable>(endpoint: String,
+                                    query: [String: Any] = [:],
+                                    contentIdentifier: String) -> Observable<T> {
     do {
       guard let url = URL(string: API)?.appendingPathComponent(endpoint),
             var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
         throw EOError.invalidURL(endpoint)
       }
-      components.queryItems = try query.flatMap { (key, value) in
+      
+      components.queryItems = try query.compactMap { (key, value) in
         guard let v = value as? CustomStringConvertible else {
           throw EOError.invalidParameter(key, value)
         }
         return URLQueryItem(name: key, value: v.description)
       }
+      
       guard let finalURL = components.url else {
         throw EOError.invalidURL(endpoint)
       }
+      
       let request = URLRequest(url: finalURL)
-      return URLSession.shared.rx.response(request: request) .map { _, data -> [String: Any] in
-        guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
-              let result = jsonObject as? [String: Any] else {
-          throw EOError.invalidJSON(finalURL.absoluteString) }
-        return result
-      }
+      
+      return URLSession.shared.rx.response(request: request)
+        .map { (result: (response: HTTPURLResponse, data: Data)) -> T in
+          let decoder = self.jsonDecoder(contentIdentifier: contentIdentifier)
+          let envelope = try decoder.decode(EOEnvelope<T>.self, from: result.data)
+          return envelope.content
+        }
     } catch {
-      return Observable.empty() }
+      return Observable.empty()
+    }
   }
 }
 
